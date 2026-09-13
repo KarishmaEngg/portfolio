50ea454b250b784349aa8f9e8f57f32d99e48e18
KarishmaEngg <karishmasingh58638@gmail.com>
2026-09-13 11:01:41 +0530

    Add full project source code

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -22,7 +22,7 @@ const projects = [
       title: "MedixFlow: Healthcare E-commerce Platform", 
       category: "Web development", 
       img: "/projects/alhawat.png", 
-      link: "https://alhawat-medical-store-9h32.vercel.app/" 
+      link: "https://medix-flow.vercel.app/" 
     },
     { 
       title: "LuxeSpace: Luxury Interior Showcase", 
@@ -53,6 +53,18 @@ const projects = [
       category: "Web design", 
       img: "https://i.pinimg.com/736x/42/3b/45/423b45b6800020d660ef01da67dbde44.jpg", 
       link: "https://muraliz-tiles.vercel.app/"
+    },
+    {
+      title:"BitNexa",
+      category:"Web design",
+      img:"https://www.reuters.com/resizer/v2/OHRVOCXQWVNYDFN2BDMRNO6B3Y.jpg?auth=d966b5290f69e1623f6b1ca95c1daf490d9b3e25e11383dff6b13a999e98b0b8&height=2400&width=1920&quality=80&smart=true",
+      link:"https://bitnexa.onrender.com/"
+    },
+    {
+      title:"Cryptonix",
+      category:"Web design",
+      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAxZoKwFgJUVUi023FeYOdyX8Dqbjb6FVzkgpyvxR24PzzhqJLIshVM4yN&s=10",
+      link:"https://cryptonix-n6el.onrender.com/"
     }
   ];
 
